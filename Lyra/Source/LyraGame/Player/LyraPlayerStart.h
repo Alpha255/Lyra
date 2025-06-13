775d398775b1f41cccec79917a81fb9c395c1dae
alpha255 <sadma@lilith.com>
2025-06-13 18:21:24 +0800

    * Add LyraPlayerStart and LyraLocalPlayer

---
+++ Lyra/Source/LyraGame/Player/LyraPlayerStart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "LyraPlayerStart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LYRAGAME_API ALyraPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+	
+};
