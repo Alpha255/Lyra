fb2d68989a4d5e881a0442c2e425f9ba8e3c81bf
alpha255 <sadma@lilith.com>
2025-07-17 15:42:47 +0800

    * Fix the missing LyraEquipmentManagerComponent of LyraCharacter

--- Lyra/Plugins/ModularGameplayActors/Source/ModularGameplayActors/Private/ModularCharacter.cpp
+++ Lyra/Plugins/ModularGameplayActors/Source/ModularGameplayActors/Private/ModularCharacter.cpp
@@ -8,14 +8,20 @@
 void AModularCharacter::PreInitializeComponents()
 {
 	Super::PreInitializeComponents();
+
+    UGameFrameworkComponentManager::AddGameFrameworkComponentReceiver(this);
 }
 
 void AModularCharacter::BeginPlay()
 {
+    UGameFrameworkComponentManager::SendGameFrameworkComponentExtensionEvent(this, UGameFrameworkComponentManager::NAME_GameActorReady);
+
 	Super::BeginPlay();
 }
 
 void AModularCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+    UGameFrameworkComponentManager::RemoveGameFrameworkComponentReceiver(this);
+
 	Super::EndPlay(EndPlayReason);
 }
