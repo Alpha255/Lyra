db59b597764b693efcfa06f5229ea289f27e9ca0
alpha255 <189815810@qq.com>
2026-03-27 14:04:58 +0800

    * Give jump ability  (WIP)

--- Lyra/Source/LyraGame/AbilitySystem/Abilities/LyraGameplayAbility.cpp
+++ Lyra/Source/LyraGame/AbilitySystem/Abilities/LyraGameplayAbility.cpp
@@ -4,6 +4,18 @@
 #include "AbilitySystem/Abilities/LyraGameplayAbility.h"
 #include "Character/LyraCharacter.h"
 
+ULyraGameplayAbility::ULyraGameplayAbility(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+    ReplicationPolicy = EGameplayAbilityReplicationPolicy::ReplicateNo;
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+    NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+    NetSecurityPolicy = EGameplayAbilityNetSecurityPolicy::ClientOrServer;
+    
+    ActivationPolicy = ELyraAbilityActivationPolicy::OnInputTriggered;
+    ActivationGroup = ELyraAbilityActivationGroup::Independent;
+}
+
 AController* ULyraGameplayAbility::GetController() const
 {
     if (CurrentActorInfo)
@@ -37,3 +49,50 @@ ALyraCharacter* ULyraGameplayAbility::GetLyraCharacter() const
 {
     return CurrentActorInfo ? Cast<ALyraCharacter>(CurrentActorInfo->AvatarActor.Get()) : nullptr;
 }
+
+bool ULyraGameplayAbility::CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo, const FGameplayTagContainer * SourceTags, const FGameplayTagContainer * TargetTags, FGameplayTagContainer * OptionalRelevantTags) const
+{
+    return false;
+}
+
+void ULyraGameplayAbility::SetCanBeCanceled(bool bCanBeCanceled)
+{}
+
+void ULyraGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo * ActorInfo, const FGameplayAbilitySpec & Spec)
+{}
+
+void ULyraGameplayAbility::OnRemoveAbility(const FGameplayAbilityActorInfo * ActorInfo, const FGameplayAbilitySpec & Spec)
+{}
+
+void ULyraGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData * TriggerEventData)
+{}
+
+void ULyraGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{}
+
+bool ULyraGameplayAbility::CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo, OUT FGameplayTagContainer * OptionalRelevantTags) const
+{
+    return false;
+}
+
+void ULyraGameplayAbility::ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{}
+
+FGameplayEffectContextHandle ULyraGameplayAbility::MakeEffectContext(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo) const
+{
+    return FGameplayEffectContextHandle();
+}
+
+void ULyraGameplayAbility::ApplyAbilityTagsToGameplayEffectSpec(FGameplayEffectSpec& Spec, FGameplayAbilitySpec* AbilitySpec) const
+{}
+
+bool ULyraGameplayAbility::DoesAbilitySatisfyTagRequirements(const UAbilitySystemComponent & AbilitySystemComponent, const FGameplayTagContainer * SourceTags, const FGameplayTagContainer * TargetTags, OUT FGameplayTagContainer * OptionalRelevantTags) const
+{
+    return false;
+}
+
+void ULyraGameplayAbility::OnPawnSet()
+{}
+
+void ULyraGameplayAbility::GetAbilitySource(FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo * ActorInfo, float& OutSourceLevel, const ILyraAbilitySourceInterface * &OutAbilitySource, AActor * &OutEffectCauser) const
+{}
--- Lyra/Source/LyraGame/AbilitySystem/Abilities/LyraGameplayAbility.h
+++ Lyra/Source/LyraGame/AbilitySystem/Abilities/LyraGameplayAbility.h
@@ -6,6 +6,24 @@
 #include "Abilities/GameplayAbility.h"
 #include "LyraGameplayAbility.generated.h"
 
+
+UENUM(BlueprintType)
+enum class ELyraAbilityActivationPolicy : uint8
+{
+    OnInputTriggered,
+    WhileInputActive,
+    OnSpawn
+};
+
+UENUM(BlueprintType)
+enum class ELyraAbilityActivationGroup : uint8
+{
+    Independent,
+    ExclusiveReplacealbe,
+    ExclusiveBlocking,
+    MAX UMETA(Hidden)
+};
+
 /**
  * 
  */
@@ -14,6 +32,62 @@ class LYRAGAME_API ULyraGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
 	
+public:
+    ULyraGameplayAbility(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+    virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, 
+        const FGameplayAbilityActorInfo* ActorInfo, 
+        const FGameplayTagContainer* SourceTags, 
+        const FGameplayTagContainer* TargetTags, 
+        FGameplayTagContainer* OptionalRelevantTags) const override;
+
+    virtual void SetCanBeCanceled(bool bCanBeCanceled) override;
+
+    virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    virtual void OnRemoveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+        const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo,
+        const FGameplayEventData* TriggerEventData) override;
+
+    virtual void EndAbility(const FGameplayAbilitySpecHandle Handle,
+        const FGameplayAbilityActorInfo* ActorInfo,
+        const FGameplayAbilityActivationInfo ActivationInfo,
+        bool bReplicateEndAbility,
+        bool bWasCancelled) override;
+
+    virtual bool CheckCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+    virtual void ApplyCost(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+
+    virtual FGameplayEffectContextHandle MakeEffectContext(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo) const override;
+
+    virtual void ApplyAbilityTagsToGameplayEffectSpec(FGameplayEffectSpec& Spec, FGameplayAbilitySpec* AbilitySpec) const override;
+
+    virtual bool DoesAbilitySatisfyTagRequirements(const UAbilitySystemComponent& AbilitySystemComponent,
+        const FGameplayTagContainer* SourceTags = nullptr,
+        const FGameplayTagContainer* TargetTags = nullptr,
+        OUT FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
+
+    virtual void OnPawnSet();
+
+    virtual void GetAbilitySource(FGameplayAbilitySpecHandle Handle, 
+        const FGameplayAbilityActorInfo* ActorInfo, 
+        float& OutSourceLevel, 
+        const class ILyraAbilitySourceInterface*& OutAbilitySource, 
+        AActor*& OutEffectCauser) const;
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Ability", DisplayName = "OnAbilityAdded")
+    void K2_OnAbilityAdded();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Ability", DisplayName = "OnAbilityRemoved")
+    void K2_OnAbilityRemoved();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Ability", DisplayName = "OnPawnSet")
+    void K2_OnPawnSet();
 
 public:
     UFUNCTION(BlueprintCallable, Category = "Lyra|Ability")
@@ -21,4 +95,16 @@ public:
 
     UFUNCTION(BlueprintCallable, Category = "Lyra|Ability")
     class ALyraCharacter* GetLyraCharacter() const;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Lyra|Ability Activation")
+    ELyraAbilityActivationPolicy ActivationPolicy;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Lyra|Ability Activation")
+    ELyraAbilityActivationGroup ActivationGroup;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Debug")
+    bool bLogOnCancel = false;
+
+    TSubclassOf<class ULyraCameraMode> ActiveCameraMode = nullptr;
 };
