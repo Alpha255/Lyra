bdc3ff11e27096afa19062bd39fcf483fa0339c0
Alpha255 <189815810@qq.com>
2026-04-06 23:18:35 +0800

    * Add reticle UI (WIP)
    * Need UIExtension plugin ???

--- Lyra/Source/LyraGame/UI/LyraHUDLayout.cpp
+++ Lyra/Source/LyraGame/UI/LyraHUDLayout.cpp
@@ -3,3 +3,58 @@
 
 #include "UI/LyraHUDLayout.h"
 
+#include "CommonUISettings.h"
+#include "GameFramework/InputDeviceSubsystem.h"
+#include "GameFramework/InputSettings.h"
+#include "GenericPlatform/GenericPlatformInputDeviceMapper.h"
+#include "Input/CommonUIInputTypes.h"
+#include "ICommonUIModule.h"
+#include "NativeGameplayTags.h"
+#include "UI/LyraActivatableWidget.h"
+
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_UI_LAYER_MENU, "UI.Layer.Menu");
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_UI_ACTION_ESCAPE, "UI.Action.Escape");
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Platform_Trait_Input_PrimarlyController, "Platform.Trait.Input.PrimarlyController");
+
+ULyraHUDLayout::ULyraHUDLayout(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+    , SpawndControllerDisconnectScreen(nullptr)
+{
+    PlatformControllerDisconnectTag.AddTag(TAG_Platform_Trait_Input_PrimarlyController);
+
+    RegisterUIActionBinding(FBindUIActionArgs(FUIActionTag::ConvertChecked(TAG_UI_ACTION_ESCAPE), false, FSimpleDelegate::CreateUObject(this, &ThisClass::OnEscapeAction)));
+}
+
+void ULyraHUDLayout::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+}
+
+void ULyraHUDLayout::NativeDestruct()
+{}
+
+void ULyraHUDLayout::OnEscapeAction()
+{}
+
+void ULyraHUDLayout::OnInputDeviceConnectionChanged(EInputDeviceConnectionState ConnectionState, FPlatformUserId UserId, FInputDeviceId InputDeviceId)
+{}
+
+void ULyraHUDLayout::OnInputDevicePairingChanged(FInputDeviceId InputDeviceId, FPlatformUserId NewUserId, FPlatformUserId OldUserId)
+{}
+
+void ULyraHUDLayout::NotifyControllerStateChangeForDisconectScreen()
+{}
+
+void ULyraHUDLayout::ProcessControllerDevicesChangedForDisconnectScreen()
+{}
+
+bool ULyraHUDLayout::ShouldDisplayControllerDisconnectScreen() const
+{
+    return false;
+}
+
+void ULyraHUDLayout::ShowControllerDisconnectMenu_Implementation()
+{}
+
+void ULyraHUDLayout::HideControllerDisconnectMenu_Implementation()
+{}
--- Lyra/Source/LyraGame/UI/LyraHUDLayout.h
+++ Lyra/Source/LyraGame/UI/LyraHUDLayout.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "UI/LyraActivatableWidget.h"
+#include "GameplayTagContainer.h"
 #include "LyraHUDLayout.generated.h"
 
 /**
@@ -14,4 +15,42 @@ class LYRAGAME_API ULyraHUDLayout : public ULyraActivatableWidget
 {
 	GENERATED_BODY()
 	
+public:
+    ULyraHUDLayout(const FObjectInitializer& ObjectInitializer);
+
+    virtual void NativeOnInitialized() override;
+    virtual void NativeDestruct() override;
+
+protected:
+    void OnEscapeAction();
+
+    void OnInputDeviceConnectionChanged(EInputDeviceConnectionState ConnectionState, FPlatformUserId UserId, FInputDeviceId InputDeviceId);
+
+    void OnInputDevicePairingChanged(FInputDeviceId InputDeviceId, FPlatformUserId NewUserId, FPlatformUserId OldUserId);
+
+    void NotifyControllerStateChangeForDisconectScreen();
+
+    virtual void ProcessControllerDevicesChangedForDisconnectScreen();
+
+    virtual bool ShouldDisplayControllerDisconnectScreen() const;
+
+    UFUNCTION(BlueprintNativeEvent, Category = "Controller Disconnect Menu")
+    void ShowControllerDisconnectMenu();
+
+    UFUNCTION(BlueprintNativeEvent, Category = "Controller Disconnect Menu")
+    void HideControllerDisconnectMenu();
+
+    UPROPERTY(EditDefaultsOnly)
+    TSoftClassPtr<UCommonActivatableWidget> EscapeMenuClass;
+
+    //UPROPERTY(EditDefaultsOnly, Category = "Controller Disconnect Menu")
+    //TSubclassOf<class ULyraControllerDisconnectedScreen> ControllerDisconnectedScreen;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Controller Disconnect Menu")
+    FGameplayTagContainer PlatformControllerDisconnectTag;
+    
+    UPROPERTY(Transient)
+    TObjectPtr<UCommonActivatableWidget> SpawndControllerDisconnectScreen;
+
+    FTSTicker::FDelegateHandle RequestProcessControllerStateDelegate;
 };
