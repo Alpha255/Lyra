79d346e17e0f103090f3f6d9a2db87123c036417
Alpha255 <189815810@qq.com>
2026-04-10 00:01:19 +0800

    * Add reticle UI (WIP), point widgets (WIP)

--- Lyra/Source/LyraGame/UI/Widgets/LyraPointWidget.h
+++ Lyra/Source/LyraGame/UI/Widgets/LyraPointWidget.h
@@ -4,21 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "Components/DynamicEntryBoxBase.h"
+#include "GameplayTagContainer.h"
 #include "LyraPointWidget.generated.h"
 
+struct FPointWidgetRequest;
+
 UENUM(BlueprintType)
-enum class EPointMatch : uint8
+enum class EGameplayTagMatchPolicy : uint8
 {
     ExactMatch,
     PartialMatch
 };
 
 UENUM(BlueprintType)
-enum class EAction : uint8
+enum class EPointWidgetAction : uint8
 {
     Added,
     Removed
 };
+
+DECLARE_DELEGATE_TwoParams(FOnPointWidgetsActionDelegate, EPointWidgetAction Action, const FPointWidgetRequest& Request);
 
 /**
  * 
@@ -28,4 +33,23 @@ class LYRAGAME_API ULyraPointWidget : public UDynamicEntryBoxBase
 {
 	GENERATED_BODY()
 	
+public:
+    ULyraPointWidget(const FObjectInitializer& ObjectInitializer);
+
+    virtual TSharedRef<SWidget> RebuildWidget() override;
+    virtual void ReleaseSlateResources(bool bReleaseChildren) override;
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    FGameplayTag Tag;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    EGameplayTagMatchPolicy MatchPolicy = EGameplayTagMatchPolicy::ExactMatch;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UI Extension")
+    TArray<TObjectPtr<UClass>> ValidWidgetClasses;
+private:
+    void RegisterSubWidgets();
+    void RegisterSubWidgetsWithPlayerState(class UCommonLocalPlayer* LocalPlayer, APlayerState* PlayerState);
+
+    void OnWidgetAction(EPointWidgetAction Action, const FPointWidgetRequest& Request);
 };
